10800dc2714465a7799997d48efa4890afe56c29
aloor <djzdcw@yeah.net>
2017-10-11 09:33:28 +0800

    config cnfonts

--- emacs_config/lisp/compat.el
+++ emacs_config/lisp/compat.el
@@ -6,11 +6,16 @@
       (equal "windows-nt" (symbol-name system-type))
       1 nil))
 
+(if
+    (windows-nt?)
+    (
 ;; 引用中文字体插件
 (require 'cnfonts)
 ;; 让 cnfonts 随着 Emacs 自动生效。
 (cnfonts-enable)
 ;; 让 spacemacs mode-line 中的 Unicode 图标正确显示。
 (cnfonts-set-spacemacs-fallback-fonts)
+     ))
+
 
 (provide 'compat)
